74d8aa879bf9ae214dc756a86b32fff90f2e7aac
Aaron-Orellana <amorellanasaldias@gmail.com>
2025-09-30 19:45:39 -0300

    Tareas Aaron

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Perfil(models.Model):
+    tipo_perfil = models.ForeignKey('accounts.TipoPerfil', on_delete=models.PROTECT, db_column='TipoPerfil_id', related_name="perfiles")
+    first_session = models.BooleanField(default=True)
+    token_app_session = models.CharField(max_length=255,blank=True,null=True) 
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
+        ordering = ['tipo_perfil']
+    def __str__(self):
+        return f"{self.tipo_perfil} - Primera sesión: {self.first_session}"
--- surveys/models.py
+++ surveys/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+# Create your models here.
+class Encuesta(models.Model):
+    titulo = models.CharField(max_length=240)
+    descripcion = models.TextField()
+    prioridad = models.CharField(max_length=20)
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Encuesta'
+        verbose_name_plural = 'Encuestas'
+        ordering = ['prioridad','fecha_creacion']
+    
+    def __str__(self):
+        return f"{self.titulo} ({'Activa' if self.estado else 'Inactiva'})"
+
 class Respuesta(models.Model):
     respuesta_texto = models.TextField(blank=True, null=True)
     valor = models.IntegerField(blank=True, null=True)
@@ -28,7 +44,7 @@ class Respuesta(models.Model):
 class Pregunta(models.Model):
     pregunta_id = models.AutoField(primary_key=True) 
     nombre = models.CharField(max_length=200)
-    encuesta = models.IntegerField(db_column='Encuesta_id')#models.ForeignKey(Encuesta, on_delete=models.PROTECT, db_column='Encuesta_id', related_name='Pregunta')  # Clave foránea a Encuesta
+    encuesta = models.ForeignKey(Encuesta, on_delete=models.PROTECT, db_column='Encuesta_id', related_name='Pregunta')  # Clave foránea a Encuesta
     tipo = models.CharField(max_length=50) 
 
     def __str__(self):
@@ -37,6 +53,4 @@ class Pregunta(models.Model):
     class Meta:
         verbose_name = 'Pregunta'
         verbose_name_plural = 'Preguntas'
-
-# Create your models here.
 
