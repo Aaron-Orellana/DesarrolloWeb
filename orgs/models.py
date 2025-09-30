9b960d0f95d02160036d4111e67433a14a9ccabf
Constanza <cauchupanconstanza@gmail.com>
2025-09-30 18:33:26 -0300

    historias constanza listo

--- orgs/models.py
+++ orgs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=150)
+
+    direccion = models.ForeignKey(
+        'Direccion', 
+        on_delete=models.PROTECT,
+        db_column='Direccion_id',
+        related_name='Departamento'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["direccion", "nombre"], name="unique_departamento_por_direccion")
+        ]
+
+    def __str__(self):
+        return f"{self.nombre} ({self.direccion})"
 # Create your models here.
--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+class Respuesta(models.Model):
+    respuesta_texto = models.TextField(blank=True, null=True)
+    valor = models.IntegerField(blank=True, null=True)
+
+
+    pregunta = models.ForeignKey(
+        'Pregunta',
+        on_delete=models.PROTECT,
+        db_column='Pregunta_id',
+        related_name='Respuesta'
+    )
+    solicitudincidencia = models.ForeignKey(
+        'tickets.SolicitudIncidencia',
+        on_delete=models.PROTECT,
+        db_column='Solicitud_Incidencia_id',
+        related_name='Respuesta'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["solicitud", "pregunta"], name="unique_respuesta_por_pregunta_solicitud")
+        ]
+
+    def __str__(self):
+        return f"{self.solicitud} - {self.pregunta}"
 # Create your models here.
