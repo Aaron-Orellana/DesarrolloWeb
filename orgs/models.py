7cc964466187dec5dc24e0d03e43454e5202baeb
Angel-Barraza <angelbarraza6817@gmail.com>
2025-09-30 19:23:41 -0300

    Agregada tabla pregunta y cuadrilla en surveys y orgs respectivamente.

--- orgs/models.py
+++ orgs/models.py
@@ -17,4 +17,17 @@ class Departamento(models.Model):
 
     def __str__(self):
         return f"{self.nombre} ({self.direccion})"
+class Cuadrilla(models.Model):
+    cuadrilla_id = models.AutoField(primary_key=True) 
+    nombre = models.CharField(max_length=100) 
+    estado = models.CharField(max_length=20, default='Activo') 
+    departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT, db_column='Departamento_id', related_name='Cuadrilla')
+    usuario = models.IntegerField(db_column='Usuario_id') #models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Cuadrilla'
+        verbose_name_plural = 'Cuadrillas'
 # Create your models here.
--- surveys/models.py
+++ surveys/models.py
@@ -25,4 +25,18 @@ class Respuesta(models.Model):
 
     def __str__(self):
         return f"{self.solicitud} - {self.pregunta}"
+class Pregunta(models.Model):
+    pregunta_id = models.AutoField(primary_key=True) 
+    nombre = models.CharField(max_length=200)
+    encuesta = models.IntegerField(db_column='Encuesta_id')#models.ForeignKey(Encuesta, on_delete=models.PROTECT, db_column='Encuesta_id', related_name='Pregunta')  # Clave foránea a Encuesta
+    tipo = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Pregunta'
+        verbose_name_plural = 'Preguntas'
+
 # Create your models here.
+
