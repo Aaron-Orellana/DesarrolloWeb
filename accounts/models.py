858c7e750f2a42dea2c3ccc6cbd02b949359654f
Cielo <vinazzaleon@gmail.com>
2025-10-01 11:15:00 -0300

    Arreglo en Usuario

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 class TipoPerfil(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
@@ -23,9 +22,6 @@ class Usuario(models.Model):
     email = models.EmailField(unique=True)   # Email único
     telefono = models.CharField(max_length=20, blank=True, null=True)
     perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name="usuarios")
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
-
+    
     def __str__(self):
         return f"{self.email} - {self.perfil.nombre}"
