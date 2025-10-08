b039845d310bd7e4cf5ff8f8ecede796704bfe94
luciano <lucianoarca27@gmail.com>
2025-10-07 23:22:43 -0300

    Actualizar modelos de orgs, corregir Cuadrilla, agregar fixtures y migración

--- orgs/models.py
+++ orgs/models.py
@@ -32,8 +32,17 @@ class Cuadrilla(models.Model):
     cuadrilla_id = models.AutoField(primary_key=True) 
     nombre = models.CharField(max_length=100) 
     estado = models.CharField(max_length=20, default='Activo') 
-    departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT, db_column='Departamento_id', related_name='Cuadrilla')
-    usuario = models.IntegerField(db_column='Usuario_id') #models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
+    departamento = models.ForeignKey(
+        Departamento,
+        on_delete=models.PROTECT,
+        db_column='Departamento_id',
+        related_name='Cuadrilla'
+    )
+    usuario = models.IntegerField(
+        db_column='Usuario_id',
+        null=True,      # permite valores nulos
+        blank=True
+    )  
 
     def __str__(self):
         return self.nombre
@@ -41,6 +50,7 @@ class Cuadrilla(models.Model):
     class Meta:
         verbose_name = 'Cuadrilla'
         verbose_name_plural = 'Cuadrillas'
+
 # Create your models here.
 
 #Creacion tabla Direccion
