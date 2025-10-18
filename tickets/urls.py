da679f07a392f7c3c6e9c21836097af778e903b4
Angel-Barraza <angelbarraza6817@gmail.com>
2025-10-18 19:06:04 -0300

    Se añade funcionalidad de evidencias (validacion cuadrilla pendiente)

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
+from tickets import views
+print(f"Type of MultimediaListView: {type(views.MultimediaListView)}")
+print(f"Type of MultimediaCreateView: {type(views.MultimediaCreateView)}")
 
 urlpatterns = [
     path('solicitud/', views.solicitud_listar, name='solicitud_listar'),
     path('solicitud/crear/', views.solicitud_crear, name='solicitud_crear'),
     path('solicitud/editar/<int:solicitud_incidencia_id>/', views.solicitud_editar, name='solicitud_editar'),
+    path('solicitud/<int:solicitud_incidencia_id>/multimedia/', views.MultimediaListView.as_view(), name='multimedia_listar'),
+    path('solicitud/<int:solicitud_incidencia_id>/multimedia/crear/', views.MultimediaCreateView.as_view(), name='multimedia_crear'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import SolicitudIncidencia
-from orgs.models import Profile
+from .models import SolicitudIncidencia, Multimedia
+from orgs.models import Profile, Cuadrilla
 from .forms import SolicitudIncidenciaForm 
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.core.exceptions import PermissionDenied
+from django.views.generic.edit import CreateView
 @login_required
 def solicitud_listar(request):
     try:
@@ -52,3 +57,49 @@ def solicitud_editar(request, solicitud_incidencia_id):
     else:
         form = SolicitudIncidenciaForm(instance=solicitud)
     return render(request, 'tickets/solicitud_editar.html', {'form': form, 'solicitud': solicitud})
+
+
+class MultimediaListView(LoginRequiredMixin, ListView):
+    model = Multimedia
+    template_name = 'tickets/multimedia_listar.html'
+    context_object_name = 'multimedias'
+
+    def get_queryset(self):
+        solicitud_id = self.kwargs['solicitud_incidencia_id']
+        return Multimedia.objects.filter(
+            solicitud_incidencia_id=solicitud_id
+        ).select_related('solicitud_incidencia')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['solicitud'] = SolicitudIncidencia.objects.get(pk=self.kwargs['solicitud_incidencia_id'])
+        return context
+
+
+class MultimediaCreateView(LoginRequiredMixin, CreateView):
+    model = Multimedia
+    fields = ['archivo', 'tipo']
+    template_name = 'tickets/multimedia_crear.html'
+
+    def get_initial(self):
+        solicitud_incidencia_id = self.kwargs['solicitud_incidencia_id']
+        return {'solicitud_incidencia': solicitud_incidencia_id}
+
+    def form_valid(self, form):
+        profile = self.request.user.profile
+        solicitud = SolicitudIncidencia.objects.get(pk=self.kwargs['solicitud_incidencia_id'])
+        #if not Cuadrilla.objects.filter(profile=profile, estado=True, pk=solicitud.cuadrilla_id).exists():
+        #   raise PermissionDenied("No estás asignado a la cuadrilla de esta solicitud o la cuadrilla no está activa.")
+
+        multimedia = form.save(commit=False)
+        multimedia.solicitud_incidencia = solicitud
+        multimedia.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('multimedia_listar', kwargs={'solicitud_incidencia_id': self.kwargs['solicitud_incidencia_id']})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['solicitud'] = SolicitudIncidencia.objects.get(pk=self.kwargs['solicitud_incidencia_id'])
+        return context
